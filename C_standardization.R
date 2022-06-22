907c41c203425c577518bf0933e5b04121f82ec2
Thiago Pinto <thiago.pinto@slu.se>
2022-06-22 12:05:53 +0200

    tp_20220622: new collumn on dataset: "Author_year"

--- C_standardization.R
+++ C_standardization.R
@@ -39,7 +39,7 @@ plot(dataset$AFS_Stock_t_ha/dataset$Depth_cm, dataset$Reference_depth, ylim=c(15
      col=AFS_palette[dataset$AFS_classification], 
      pch=as.numeric(dataset$AFS_classification), xlim=c(0,4))
 
-legend("bottomright", levels(dataset$AFS_classification), col=AFS_palette, pch=c(1:8), bty="n")
+legend("bottomright", levels(dataset$AFS_classification), col=AFS_palette,cex=0.6, pch=c(1:8), bty="n")
 
 #text(dataset$AFS_Stock_t_ha/dataset$Depth_cm, dataset$Reference_depth+5, cex=0.6, dataset$ID)
 
